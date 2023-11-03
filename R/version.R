98431273eda7bad65c8bd313b1668997718dac71
Barret Schloerke <barret@posit.co>
2023-11-03 10:38:21 -0400

    bump assets version to v0.2.2

--- R/version.R
+++ R/version.R
@@ -1,3 +1,3 @@
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION <- "0.2.1"
+SHINYLIVE_ASSETS_VERSION <- "0.2.2"
 SHINYLIVE_R_VERSION <- as.character(utils::packageVersion("shinylive"))
