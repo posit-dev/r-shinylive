4e1045dc0ce560d425ab28cb4a3c9bfd50f685a7
Winston Chang <winston@stdout.org>
2023-11-30 08:59:53 -0600

    Bump shinylive web assets to 0.2.3 (#38)

--- R/version.R
+++ R/version.R
@@ -1,3 +1,3 @@
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION <- "0.2.1"
+SHINYLIVE_ASSETS_VERSION <- "0.2.3"
 SHINYLIVE_R_VERSION <- as.character(utils::packageVersion("shinylive"))
