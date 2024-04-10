2039ee5ffca43fee2649338ad4093acdc39947c8
Garrick Aden-Buie <garrick@adenbuie.com>
2024-04-10 13:44:52 -0400

    Add `url_encode_dir()` and lzstring compression/decompression

---
+++ R/url.R
@@ -0,0 +1,98 @@
+url_encode_dir <- function(
+  dir,
+  language = c("auto", "r", "py"),
+  mode = c("editor", "app"),
+  hide_header = FALSE,
+  exclude = "rsconnect/"
+) {
+  check_v8_installed()
+
+  stopifnot(fs::dir_exists(dir))
+
+  language <- rlang::arg_match(language)
+  mode <- rlang::arg_match(mode)
+  hide_header <- hide_header && mode == "app"
+
+  files <- fs::dir_ls(dir, recurse = TRUE, type = "file", regexp = exclude, invert = TRUE)
+  files <- fs::path_abs(files)
+  files <- files[!grepl("^(\\.|_)", fs::path_file(files))]
+
+  rgx_app <- switch(
+    language,
+    r = "(^app|(ui|server)).*\\.[Rr])",
+    py = "^app.+\\.py$",
+    auto = "^app|((ui|server)\\.[Rr])"
+  )
+
+  idx_app <- which(grepl(rgx_app, fs::path_file(files)))
+  if (length(idx_app) == 0) {
+    stop(
+      switch(
+        language,
+        r = "No app.R, ui.R or server.R found in ",
+        py = "No app.py found in ",
+        auto = "No app.R, app.py or ui/server.R found in "
+      ),
+      dir
+    )
+  }
+
+  idx_app <- idx_app[1]
+  path_root <- fs::path_dir(files[idx_app])
+  files <- c(files[idx_app], files[-idx_app])
+
+  if (language == "auto") {
+    language <- tolower(fs::path_ext(files[1]))
+  }
+
+  names <- fs::path_rel(files, path_root)
+
+  bundle <- unname(Map(as_file_list, files, names))
+
+  sprintf(
+    "https://%s/%s/%s/#%scode=%s",
+    getOption("shinylive.host", "shinylive.io"),
+    language,
+    mode,
+    if (hide_header) "h=0&" else "",
+    lzstring_compress_uri(bundle)
+  )
+}
+
+
+as_file_list <- function(path, name = fs::path_file(path), type = NULL) {
+  if (is.null(type)) {
+    ext <- tolower(fs::path_ext(path))
+    type <- if (ext %in% text_file_extensions()) "text" else "binary"
+  } else {
+    rlang::arg_match(type, c("text", "binary"))
+  }
+
+  content <-
+    if (type == "text") {
+      read_utf8(path)
+    } else {
+      rlang::check_installed("base64enc", "for binary file encoding.")
+      base64enc::base64encode(read_raw(path))
+    }
+
+  ret <- list(name = name, content = content)
+  if (type == "binary") ret$type <- "binary"
+
+  ret
+}
+
+text_file_extensions <- function() {
+  c(
+    "r", "rmd", "rnw", "rpres", "rhtml", "qmd",
+    "py", "ipynb", "js", "ts", "jl",
+    "html", "css", "scss", "less", "sass",
+    "tex", "txt", "md", "markdown", "html", "htm",
+    "json", "yml", "yaml", "xml", "svg",
+    "sh", "bash", "zsh", "fish", "bat", "cmd",
+    "sql", "csv", "tsv", "tab",
+    "log", "dcf", "ini", "cfg", "conf", "properties", "env", "envrc",
+    "gitignore", "gitattributes", "gitmodules", "gitconfig", "gitkeep",
+    "htaccess", "htpasswd", "htgroups", "htdigest"
+  )
+}
--- R/utils.R
+++ R/utils.R
@@ -54,6 +54,26 @@ drop_nulls_rec <- function(x) {
   }
 }
 
+raw_to_utf8 <- function(data) {
+  res <- rawToChar(data)
+  Encoding(res) <- "UTF-8"
+  res
+}
+
+read_raw <- function(file) {
+  readBin(file, "raw", n = file.info(file, extra_cols = FALSE)$size)
+}
+
+# Read file as UTF-8
+read_utf8 <- function(file) {
+  res <- read_raw(file)
+  raw_to_utf8(res)
+}
+
+# Write text as UTF-8
+write_utf8 <- function(text, ...) {
+  writeBin(charToRaw(enc2utf8(text)), ...)
+}
 
 
 # """Returns a function that can be used as a copy_function for shutil.copytree.
