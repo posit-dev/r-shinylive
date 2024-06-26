dafa82ea4f759a2eef2bc3cc6e32d65633a1161c
Barret Schloerke <barret@posit.co>
2024-06-26 12:02:09 -0400

    docs(export(assets_version=)): Add caveat that not all `assets_version` values may work (#95)

--- R/export.R
+++ R/export.R
@@ -13,7 +13,10 @@
 #' @param package_cache Cache downloaded binary WebAssembly packages. Defaults
 #'    to `TRUE`.
 #' @param assets_version The version of the Shinylive assets to use in the
-#'    exported app. Defaults to [assets_version()].
+#'    exported app. Defaults to [assets_version()]. Note, not all custom assets
+#'    versions may work with this release of \pkg{shinylive}. Please visit the
+#'    [shinylive asset releases](https://github.com/posit-dev/shinylive/releases)
+#'    website to learn more information about the available `assets_version` values.
 #' @param ... Ignored
 #' @export
 #' @return Nothing. The app is exported to `destdir`. Instructions for serving
