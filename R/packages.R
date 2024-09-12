d5093687678820cdc4e2a310a02e84b50643ac86
George Stagg <george.stagg@posit.co>
2024-09-12 12:32:42 +0100

    Support compressed Wasm libraries on GitHub

--- R/packages.R
+++ R/packages.R
@@ -99,7 +99,7 @@ get_github_wasm_assets <- function(desc) {
 
   # Find GH release asset URLs for R library VFS image
   library_data <- Filter(function(item) {
-    item$name == "library.data"
+    grepl("library.data", item$name)
   }, tags$assets)
   library_metadata <- Filter(function(item) {
     item$name == "library.js.metadata"
