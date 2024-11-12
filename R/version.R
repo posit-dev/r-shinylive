f581ab26173d044890c707f2927fcbe128735239
Garrick Aden-Buie <garrick@adenbuie.com>
2024-11-12 16:35:04 -0500

    chore: Use shinylive assets v0.9.1

--- R/version.R
+++ R/version.R
@@ -1,4 +1,4 @@
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION <- "0.9.0"
+SHINYLIVE_ASSETS_VERSION <- "0.9.1"
 SHINYLIVE_R_VERSION <- as.character(utils::packageVersion("shinylive"))
 WEBR_R_VERSION <- "4.4.1"
