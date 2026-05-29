04367c89ef37bdd5aea4b33621626668c10a3da2
George Stagg <george.stagg@posit.co>
2026-05-29 10:24:41 +0100

    Update to webR v0.6.0 and latest shinylive assets

--- R/version.R
+++ R/version.R
@@ -1,4 +1,4 @@
 # This is the version of the Shinylive assets to use.
-SHINYLIVE_ASSETS_VERSION <- "0.10.10"
+SHINYLIVE_ASSETS_VERSION <- "0.10.11"
 SHINYLIVE_R_VERSION <- as.character(utils::packageVersion("shinylive"))
-WEBR_R_VERSION <- "4.5.1"
+WEBR_R_VERSION <- "4.6.0"
